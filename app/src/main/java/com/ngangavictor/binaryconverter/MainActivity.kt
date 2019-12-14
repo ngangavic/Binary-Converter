8c6d7f1b1d034477f077ccd86cc3b0df2766b983
ngangavic <ngangavictor10@gmail.com>
2019-12-14 11:49:32 +0300

    letter search working

--- app/src/main/java/com/ngangavictor/binaryconverter/MainActivity.kt
+++ app/src/main/java/com/ngangavictor/binaryconverter/MainActivity.kt
@@ -1,28 +1,78 @@
 package com.ngangavictor.binaryconverter
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
 import android.widget.AdapterView
 import android.widget.EditText
 import android.widget.Spinner
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var editText: EditText
     lateinit var spinner: Spinner
-    lateinit var textViewResult:TextView
-    lateinit var type:String
+    lateinit var textViewResult: TextView
+    lateinit var type: String
+
+    var binary = arrayOf(
+        "01100001",
+        "01100010",
+        "01100011",
+        "01100100",
+        "01100101",
+        "01100110",
+        "01100111",
+        "01101000",
+        "01101001",
+        "01101010",
+        "01101011",
+        "01101100",
+        "01101101",
+        "01101110",
+        "01101111",
+        "01110000",
+        "01110001",
+        "01110010",
+        "01110011",
+        "01110100",
+        "01110101",
+        "01110110",
+        "01110111",
+        "01111000",
+        "01111001",
+        "01111010"
+        )
+
+    var smallLetters= arrayOf("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z")
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        editText=findViewById(R.id.editText)
-        spinner=findViewById(R.id.spinner)
-        textViewResult=findViewById(R.id.textViewResult)
+        editText = findViewById(R.id.editText)
+        spinner = findViewById(R.id.spinner)
+        textViewResult = findViewById(R.id.textViewResult)
 
+        editText.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                getBinary("Binary Coded Decimal",s.toString())
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                getBinary("Binary Coded Decimal","a")
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                getBinary("Binary Coded Decimal",s.toString())
+            }
+
+        })
+        
         spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onNothingSelected(parent: AdapterView<*>?) {
 
@@ -36,27 +86,32 @@ class MainActivity : AppCompatActivity() {
             ) {
                 if (parent != null) {
                     type = parent.getItemAtPosition(position).toString()
-                    if(type=="Binary Coded Decimal"){
-                        getBinary("Binary Coded Decimal")
-                    }else{
-                        getBinary("nothing")
-                    }
+//                    if (type == "Binary Coded Decimal") {
+//                        getBinary("Binary Coded Decimal",editText.text.toString())
+//                    } else {
+//                        getBinary("nothing","")
+//                    }
                 }
             }
 
         }
     }
 
-    private fun getBinary(convertType:String){
-        if (convertType=="Binary Coded Decimal") {
-            val binaryList = resources.getStringArray(R.array.binary)[10]
-            textViewResult.text = binaryList.toString()
-           // val a = convertType in arrayOf(binaryList)
-        }else{
-            textViewResult.text="No result"
+    private fun getBinary(convertType: String,letter:String) {
+        try {
+            if (convertType == "Binary Coded Decimal") {
+                //smallLetters.indexOf(letter).toString()
+                //textViewResult.text = binary.get(20)
+                val a = binary.get(smallLetters.indexOf(letter))
+                textViewResult.text = a.toString()
+                // val a = convertType in arrayOf(binaryList)
+            } else {
+                textViewResult.text = "No result"
+            }
+        }catch (e:Exception){
+            e.printStackTrace()
         }
     }
-
 
 
 }
