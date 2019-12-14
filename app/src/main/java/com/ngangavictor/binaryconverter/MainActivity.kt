ff8c599ba14512a9e1e76fd4d1254447c0b309e7
ngangavic <ngangavictor10@gmail.com>
2019-12-14 10:58:30 +0300

    array data

--- app/src/main/java/com/ngangavictor/binaryconverter/MainActivity.kt
+++ app/src/main/java/com/ngangavictor/binaryconverter/MainActivity.kt
@@ -2,6 +2,8 @@ package com.ngangavictor.binaryconverter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.EditText
 import android.widget.Spinner
 import android.widget.TextView
@@ -11,6 +13,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var editText: EditText
     lateinit var spinner: Spinner
     lateinit var textViewResult:TextView
+    lateinit var type:String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +22,39 @@ class MainActivity : AppCompatActivity() {
         editText=findViewById(R.id.editText)
         spinner=findViewById(R.id.spinner)
         textViewResult=findViewById(R.id.textViewResult)
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                if (parent != null) {
+                    type = parent.getItemAtPosition(position).toString()
+                    if(type=="Binary Coded Decimal"){
+                        getBinary("Binary Coded Decimal")
+                    }else{
+                        getBinary("nothing")
+                    }
+                }
+            }
+
+        }
+    }
+
+    private fun getBinary(convertType:String){
+        if (convertType=="Binary Coded Decimal") {
+            val binaryList = resources.getStringArray(R.array.binary)[10]
+            textViewResult.text = binaryList.toString()
+           // val a = convertType in arrayOf(binaryList)
+        }else{
+            textViewResult.text="No result"
+        }
     }
 
 
