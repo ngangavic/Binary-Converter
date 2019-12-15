27dbacca813ae562e6620fed6254c10495c189df
ngangavic <ngangavictor10@gmail.com>
2019-12-15 08:19:33 +0300

    word conversion working

--- app/src/main/java/com/ngangavictor/binaryconverter/MainActivity.kt
+++ app/src/main/java/com/ngangavictor/binaryconverter/MainActivity.kt
@@ -3,6 +3,7 @@ package com.ngangavictor.binaryconverter
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.EditText
@@ -17,6 +18,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var spinner: Spinner
     lateinit var textViewResult: TextView
     lateinit var type: String
+    lateinit var prev:String
 
     var binary = arrayOf(
         "01100001",
@@ -57,18 +59,32 @@ class MainActivity : AppCompatActivity() {
         editText = findViewById(R.id.editText)
         spinner = findViewById(R.id.spinner)
         textViewResult = findViewById(R.id.textViewResult)
+        prev=""
 
         editText.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(s: Editable?) {
-                getBinary("Binary Coded Decimal",s.toString())
+//                if(s!!.isNotEmpty()) {
+//                    getBinary("Binary Coded Decimal", s.toString())
+//                }else{
+//                    prev=""
+//                    textViewResult.text=""
+//                }
             }
 
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                getBinary("Binary Coded Decimal","a")
+                //getBinary("Binary Coded Decimal","a")
+
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                getBinary("Binary Coded Decimal",s.toString())
+                if(s!!.isNotEmpty()) {
+                    prev=textViewResult.text.toString()
+//                    textViewResult.text=""
+                    getBinary("Binary Coded Decimal", s.toString())
+                }else{
+                    prev=""
+                    textViewResult.text=""
+                }
             }
 
         })
@@ -100,11 +116,21 @@ class MainActivity : AppCompatActivity() {
     private fun getBinary(convertType: String,letter:String) {
         try {
             if (convertType == "Binary Coded Decimal") {
-                //smallLetters.indexOf(letter).toString()
-                //textViewResult.text = binary.get(20)
-                val a = binary.get(smallLetters.indexOf(letter))
-                textViewResult.text = a.toString()
-                // val a = convertType in arrayOf(binaryList)
+
+                var i = 0
+                Log.d("LENGHT",letter.length.toString())
+                while (i < letter.length) {
+                    getBin(letter[i])
+                    i++
+                }
+
+
+//                val a = binary.get(smallLetters.indexOf(letter))
+//
+//                val final =prev+" "+a
+//                textViewResult.text=""
+//                textViewResult.text = final
+
             } else {
                 textViewResult.text = "No result"
             }
@@ -112,6 +138,18 @@ class MainActivity : AppCompatActivity() {
             e.printStackTrace()
         }
     }
+
+    private fun getBin(find:Char){
+
+        val a = binary.get(smallLetters.indexOf(find.toString()))
+        Log.d("MAIN ACTIVITY",a)
+        val final = prev+" "+a
+        Log.d("FINAL ",final)
+        textViewResult.text=""
+        textViewResult.text=final
+
+    }
+
 
 
 }
